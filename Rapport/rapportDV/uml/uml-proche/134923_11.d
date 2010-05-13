fda17e6496a7e14f2f30675b7170ce67eaa219ee
novak.audrey <audreyn@users.noreply.github.com>
2010-05-13 16:46:09 +0000

    Dernière version du rapport final

---
+++ Rapport/rapportDV/uml/uml-proche/134923_11.d
@@ -0,0 +1,34 @@
+format 71
+
+classcanvas 128011 class_ref 142859 // MIEffet
+  draw_all_relations default hide_attributes default hide_operations default hide_getset_operations default show_members_full_definition default show_members_visibility default show_members_stereotype default show_members_multiplicity default show_members_initialization default show_attribute_modifiers default member_max_width 0 show_parameter_dir default show_parameter_name default package_name_in_tab default class_drawing_mode default drawing_language default show_context_mode default auto_label_position default show_relation_modifiers default show_relation_visibility default show_infonote default shadow default show_stereotype_properties default
+  xyz 147 22 2000
+end
+classcanvas 128139 class_ref 142987 // MMouvementFini
+  draw_all_relations default hide_attributes default hide_operations default hide_getset_operations default show_members_full_definition default show_members_visibility default show_members_stereotype default show_members_multiplicity default show_members_initialization default show_attribute_modifiers default member_max_width 0 show_parameter_dir default show_parameter_name default package_name_in_tab default class_drawing_mode default drawing_language default show_context_mode default auto_label_position default show_relation_modifiers default show_relation_visibility default show_infonote default shadow default show_stereotype_properties default
+  xyz 37 173 2000
+end
+classcanvas 128267 class_ref 143115 // MMouvementPerpetuel
+  draw_all_relations default hide_attributes default hide_operations default hide_getset_operations default show_members_full_definition default show_members_visibility default show_members_stereotype default show_members_multiplicity default show_members_initialization default show_attribute_modifiers default member_max_width 0 show_parameter_dir default show_parameter_name default package_name_in_tab default class_drawing_mode default drawing_language default show_context_mode default auto_label_position default show_relation_modifiers default show_relation_visibility default show_infonote default shadow default show_stereotype_properties default
+  xyz 354 170 2005
+end
+classcanvas 128395 class_ref 143243 // MRedimensionnement
+  draw_all_relations default hide_attributes default hide_operations default hide_getset_operations default show_members_full_definition default show_members_visibility default show_members_stereotype default show_members_multiplicity default show_members_initialization default show_attribute_modifiers default member_max_width 0 show_parameter_dir default show_parameter_name default package_name_in_tab default class_drawing_mode default drawing_language default show_context_mode default auto_label_position default show_relation_modifiers default show_relation_visibility default show_infonote default shadow default show_stereotype_properties default
+  xyz 168 170 2000
+end
+relationcanvas 128523 relation_ref 143755 // <realization>
+  from ref 128139 z 2001 to ref 128011
+  no_role_a no_role_b
+  no_multiplicity_a no_multiplicity_b
+end
+relationcanvas 128651 relation_ref 143883 // <realization>
+  from ref 128395 z 2001 to ref 128011
+  no_role_a no_role_b
+  no_multiplicity_a no_multiplicity_b
+end
+relationcanvas 128779 relation_ref 144011 // <realization>
+  from ref 128267 z 2006 to ref 128011
+  no_role_a no_role_b
+  no_multiplicity_a no_multiplicity_b
+end
+end
